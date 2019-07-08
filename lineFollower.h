99315a86931f700e8b2757dec5192d18d2e2d578
enmaibette <34512908+enmaibette@users.noreply.github.com>
2019-07-08 18:32:23 +0200

    Create lineFollower.h

---
+++ lineFollower.h
@@ -0,0 +1,57 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+Zumo32U4LCD lcd;
+Zumo32U4ButtonA buttonA;
+Zumo32U4LineSensors lineSensors;
+Zumo32U4ProximitySensors proxSensors;
+Zumo32U4Motors motors;
+
+int16_t lineSensorsValue[5] = {0, 0, 0, 0, 0};
+
+void calibrateSensors(){
+  delay(1000);
+  for(uint16_t i = 0; i < 120; i++){
+    lcd.gotoXY(0,1);
+      lcd.print(i);
+    if (i > 30 && i <= 90)
+    {
+      motors.setSpeeds(-200, 200);
+    }
+    else
+    {
+      motors.setSpeeds(200, -200);
+    }
+    lineSensors.calibrate();
+  }
+  motors.setSpeeds(0, 0);
+  lcd.clear();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  lineSensors.initFiveSensors();
+  buttonA.waitForButton();
+  calibrateSensors();
+  buttonA.waitForButton();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+    lineSensors.readCalibrated(lineSensorsValue);
+  
+      if(lineSensorsValue[2] >= 800){
+        motors.setSpeeds(150, 150);
+      }else if(lineSensorsValue[0] >= 800 && lineSensorsValue[4] < 800){ // turn left
+        motors.setSpeeds(-150, 150);
+      }else if(lineSensorsValue[4] >= 800 && lineSensorsValue[0] < 800){ // turn right
+        motors.setSpeeds(150, -150);
+      
+    }
+    
+}
